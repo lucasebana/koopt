bac07589bd2b6017017b5eb92c5de8ae58deae67
hawux5 <61750768+hawux5@users.noreply.github.com>
2020-03-16 15:29:58 +0100

    Add files via upload

---
+++ server/classe joueur.py
@@ -0,0 +1,16 @@
+class Joueur:
+    nom;
+    energie;
+    bois
+    nourriture;
+    coordonnées;
+    vitesse;
+
+    def init(self, nom, coordonnées):
+        self.nom=nom
+        self.energie=100
+        self.bois=0
+        self.nourriture=0
+        self.coordonnées=
+        self.vitesse=np.array[0,0]
+
