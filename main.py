4a6f31c7538413cea42ceb36539030e39f3a46e9
hawux5 <61750768+hawux5@users.noreply.github.com>
2020-03-16 19:09:09 +0100

    classes avec méthodes (inachevé)

---
+++ main.py
@@ -0,0 +1,12 @@
+import classe joueur.py
+import classe ressource.py
+import classe partie.py
+
+def main():
+    debut_Partie()
+    joueur_meurt(joueurs)
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ map.py
@@ -0,0 +1,11 @@
+class Map:
+    tileset = None
+    tiles = [[]]
+    objets = [None] # instances (objets)
+    animaux = [] #instances d'animaux
+
+    def statut_objets(self):
+        pass
+
+    def statut_animaux(self):
+        pass
