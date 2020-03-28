bd34572cef1610c3050da8b0919ec1c6d22caae0
Lucas <lucas@ebana.fr>
2020-03-28 01:47:31 +0100

    Développement du client
    Chargement et affichage rudimentaire d'une map créée sous le logiciel Tiled.

---
+++ client/assets/pkmn.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="pkmn" tilewidth="32" tileheight="32" tilecount="25200" columns="120">
+ <image source="tilesetpkmn2xT.png" trans="e5ece8" width="3840" height="6720"/>
+</tileset>
