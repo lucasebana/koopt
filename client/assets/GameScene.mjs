0d26da983a97437dc29e6cb06614ad3fb80b060e
Lucas <lucas@ebana.fr>
2020-03-29 17:53:57 +0200

    Simplification du code
    Réorganisation des methodes update et create de GameScene

--- client/assets/GameScene.mjs
+++ client/assets/GameScene.mjs
@@ -14,10 +14,7 @@ export class GameScene extends Phaser.Scene{
     preload(){
         this.load.spritesheet("armel", "./assets/armel.png", {frameHeight: 64, frameWidth: 64});
         this.load.image('pkm', './assets//tilesetpkmnX.png');
-        this.load.tilemapTiledJSON('map', './assets/map2.json');
-
-
-        
+        this.load.tilemapTiledJSON('map', './assets/map2.json');        
     }
     create(){
         var map = this.make.tilemap({ key: 'map' });
@@ -26,64 +23,28 @@ export class GameScene extends Phaser.Scene{
         this.layer2 = map.createStaticLayer(1, tiles, 0, 0);
 
         this.armel = new JoueurSprite(this, 30, 40, "armel", 26);
-        this.anims.create({
-            key: "stretch",
-            frameRate: 10,
-            frames: this.anims.generateFrameNumbers("armel", {
-                start: 0,
-                end: 6
-            })
-        });
-
-        this.keyboard = this.input.keyboard.addKeys("Z, Q, S, D");
-
         window.armel = this.armel;
 
-        //set smaller hitbox
-        this.armel.setSize(30, 52).setOffset(17, 12);
-        this.armel.setCollideWorldBounds(true);
+        this.keyboard = this.input.keyboard.addKeys("Z, Q, S, D");
+        
         this.cameras.main.roundPixels = true;
         this.cameras.main.startFollow(this.armel,true, 0.2, 0.2);
         this.physics.world.setBounds(0,0, map.widthInPixels, map.heightInPixels);
         
-
         this.scale.on("resize",this.resize,this)
     }
     update(){
-
-
-        this.armel.play("stretch",true)
-        if (this.keyboard.D.isDown === true) {
-            this.armel.setVelocityX(200);
-
-        }
-
-        if (this.keyboard.Z.isDown === true) {
-            this.armel.setVelocityY(-200);
-        }
-
-        if (this.keyboard.S.isDown === true) {
-            this.armel.setVelocityY(200);
-        }
-
-        if (this.keyboard.Q.isDown === true) {
-            this.armel.setVelocityX(-200);
-        }
-        if (this.keyboard.Q.isUp && this.keyboard.D.isUp) { //not moving on X axis
-            this.armel.setVelocityX(0);
-        }
-        if (this.keyboard.Z.isUp && this.keyboard.S.isUp) { //not pressing y movement
-            this.armel.setVelocityY(0);
-        }
+        this.armel.context(this)        
     }
 
     resize(gameSize,baseSize,displaySize,resolution){
         console.log("GAMESCENE RESIZE ! ")
-        var width = displaySize.width
+        /*var width = displaySize.width
         var height = baseSize.height
         var ar = displaySize.aspectRatio
         console.log(ar)
         //this.layer.ResizeWorld();
-        this.layer.setDisplaySize(width,height*ar)
+        //this.layer.setDisplaySize(width,height*ar)
+        */
     }
 }
--- client/assets/JoueurSprite.mjs
+++ client/assets/JoueurSprite.mjs
@@ -10,9 +10,45 @@ export class JoueurSprite extends Phaser.Physics.Arcade.Sprite {
         scene.physics.world.enableBody(this);
         this.setImmovable(true);
         this.hp = 10;
-        
+        scene.anims.create({
+            key: "stretch",
+            frameRate: 10,
+            frames: scene.anims.generateFrameNumbers("armel", {
+                start: 0,
+                end: 6
+            })
+        });
+
+        //set smaller hitbox
+        this.setSize(30, 52).setOffset(17, 12);
+        this.setCollideWorldBounds(true);
+    }
+    context(scene){
+        this.play("stretch",true)
+        this.update();
+        if (scene.keyboard.D.isDown === true) {
+            this.setVelocityX(200);
+        }
+
+        if (scene.keyboard.Z.isDown === true) {
+            this.setVelocityY(-200);
+        }
+
+        if (scene.keyboard.S.isDown === true) {
+            this.setVelocityY(200);
+        }
+
+        if (scene.keyboard.Q.isDown === true) {
+            this.setVelocityX(-200);
+        }
+        if (scene.keyboard.Q.isUp && scene.keyboard.D.isUp) { //not moving on X axis
+            this.setVelocityX(0);
+        }
+        if (scene.keyboard.Z.isUp && scene.keyboard.S.isUp) { //not pressing y movement
+            this.setVelocityY(0);
+        }
     }
 
 
-    
+
 }
